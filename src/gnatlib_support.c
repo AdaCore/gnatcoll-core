c3c2400e849173cda17421c26bc95ebcd1356306
Emmanuel Briot <briot@adacore.com>
2007-09-08 11:10:57 +0000

    Change profile so that we do not depend on types that might not exist on  the machine (G908-005)
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@115307 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gnatlib_support.c
+++ src/gnatlib_support.c
@@ -12,13 +12,13 @@ int gnatlib_has_mmap() {
   return 0;
 }
 
-void *mmap(void *start, size_t length, int prot, int flags,
-                  int fd, off_t offset)
+void *mmap(void *start, long length, int prot, int flags,
+                  int fd, long offset)
 {
-  return (NULL);
+  return (void*)0;
 }
 
-int munmap(void *start, size_t length) {
+int munmap(void *start, long length) {
   return 0;
 }
 
