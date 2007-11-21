c1f7c31988633758c32201953e2f8f48fcaf30ab
Emmanuel Briot <briot@adacore.com>
2007-11-21 16:15:39 +0000

    Revert "(From_PyGtk): Assume we have a widget rather than an object, so that it"
    This reverts commit 751900e9913032d7810a8d39d43b9f97cfcfac6b.
    
    
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@119463 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gtk/python/gnat-scripts-python-gtkada.adb
+++ src/gtk/python/gnat-scripts-python-gtkada.adb
@@ -75,11 +75,8 @@ package body GNAT.Scripts.Python.Gtkada is
      (Data : Callback_Data'Class;
       N    : Positive) return Glib.Object.GObject
    is
-      Stub : Gtk.Widget.Gtk_Widget_Record;
+      Stub : Glib.Object.GObject_Record;
    begin
-      --  ??? Should check wether we have a widget or an object.
-      --  Since the main goal here is to get a widget to be inserted in a GUI,
-      --  in particular the MDI, let's assume we do have a widget.
       return Get_User_Data
         (Widget_From_PyObject (Get_Param (Python_Callback_Data (Data), N)),
          Stub);
