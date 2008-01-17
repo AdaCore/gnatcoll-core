4f048ae2e678c02bfd94fd8d11652b5ba5313a08
Pascal Obry <obry@adacore.com>
2008-01-17 13:13:42 +0000

    Minor reformatting
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@120883 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gnat-scripts-shell.adb
+++ src/gnat-scripts-shell.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------
 --                               G P S                               --
 --                                                                   --
---                 Copyright (C) 2003-2007, AdaCore                  --
+--                 Copyright (C) 2003-2008, AdaCore                  --
 --                                                                   --
 -- GPS is free  software;  you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
@@ -662,7 +662,7 @@ package body GNAT.Scripts.Shell is
       Errors       : access Boolean) return String
    is
       pragma Unreferenced (Show_Command);
-      Err    : aliased Boolean;
+      Err         : aliased Boolean;
       Old_Console : constant Virtual_Console := Script.Console;
    begin
       if Console /= null then
