1fb0d01b8faacdc2a451cd764a7a080de36f1ebd
Arnaud Charlet <charlet@adacore.com>
2008-08-29 15:43:38 +0000

    Undo previous change, didn't have any effect on minsk. Part of G928-005
    git-svn-id: svn+ssh://svn.eu/Dev/trunk/gps@128211 936e1b1b-40f2-da11-902a-00137254ae57

--- src/python/gnatcoll-scripts-python.adb
+++ src/python/gnatcoll-scripts-python.adb
@@ -288,8 +288,6 @@ package body GNATCOLL.Scripts.Python is
         & "         sys.stderr.write = __gps_saved_stderr" & ASCII.LF
         & "      except: pass" & ASCII.LF
         & ASCII.LF;
-      Disable_GC     : constant String := "import gc" & ASCII.LF
-        & "gc.disable()" & ASCII.LF;
 
       Main_Module    : PyObject;
       Sigint         : constant Integer := 2;
@@ -355,7 +353,6 @@ package body GNATCOLL.Scripts.Python is
       --  of commands
 
       Tmp := PyRun_SimpleString (Init_Output);
-      Tmp := PyRun_SimpleString (Disable_GC);
 
       Result := Run_Command
         (Script,
