38fad06fc2f46cc4be8f5ceaea48b734adcf2728
Jerome Lambourg <lambourg@adacore.com>
2009-04-09 14:56:01 +0000

    Major reorganisation of the Virtual Files handling and of the remote mode  configuration management. Major VFSification of GPS: GPS should now be using Virtual Files almost  everywhere possible.
    Part of I325-007
    
    
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@142509 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ src/gnatcoll-io-native-codec.adb
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------
+--                          G N A T C O L L                          --
+--                                                                   --
+--                  Copyright (C) 2008-2009, AdaCore                 --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  Windows version: all paths are already utf-8 encoded by mingw
+--  Unix version: todo, would require interfacing with 'locale'
+
+separate (GNATCOLL.IO.Native)
+package body Codec is
+
+   -------------
+   -- To_UTF8 --
+   -------------
+
+   function To_UTF8 (Path : FS_String) return String is
+   begin
+      return String (Path);
+   end To_UTF8;
+
+   ---------------
+   -- From_UTF8 --
+   ---------------
+
+   function From_UTF8 (Path : String) return FS_String is
+   begin
+      return FS_String (Path);
+   end From_UTF8;
+
+end Codec;
