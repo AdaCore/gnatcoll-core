c2285964325b833d08a016860a3a27a6996a6ac8
Emmanuel Briot <briot@adacore.com>
2010-12-22 10:16:11 +0000

    (GNATCOLL.Config): new package, for JA21-009 Bump version to 1.4w, since 1.3 was branched in the ide-2010 branch
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@168579 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ src/gnatcoll-config.adb
@@ -0,0 +1,349 @@
+-----------------------------------------------------------------------
+--                          M O D E L I N G                          --
+--                                                                   --
+--                 Copyright (C) 2010, AdaCore                       --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+with Ada.Strings.Fixed;         use Ada.Strings.Fixed;
+with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
+with GNAT.OS_Lib;               use GNAT.OS_Lib;
+with GNATCOLL.Mmap;             use GNATCOLL.Mmap;
+
+package body GNATCOLL.Config is
+   use String_Maps;
+
+   -------------------
+   -- Set_System_Id --
+   -------------------
+
+   procedure Set_System_Id (Self : in out Config_Parser; System_ID : String) is
+   begin
+      Self.System_ID := To_Unbounded_String (System_ID);
+   end Set_System_Id;
+
+   ----------------
+   -- As_Integer --
+   ----------------
+
+   function As_Integer (Self : Config_Parser) return Integer is
+   begin
+      return Integer'Value (Value (Config_Parser'Class (Self)));
+   end As_Integer;
+
+   ----------------
+   -- As_Boolean --
+   ----------------
+
+   function As_Boolean (Self : Config_Parser) return Boolean is
+   begin
+      return Boolean'Value (Value (Config_Parser'Class (Self)));
+   end As_Boolean;
+
+   ----------------------
+   -- As_Absolute_File --
+   ----------------------
+
+   function As_Absolute_File (Self : Config_Parser) return String is
+      Val : constant String := Value (Config_Parser'Class (Self));
+   begin
+      if Val = "" then
+         return "";
+      elsif Val (Val'First) = '/' then
+         return Val;
+      else
+         return Normalize_Pathname (Val, To_String (Self.System_ID));
+      end if;
+   end As_Absolute_File;
+
+   ---------------------
+   -- As_Absolute_Dir --
+   ---------------------
+
+   function As_Absolute_Dir (Self : Config_Parser) return String is
+      V : constant String := As_Absolute_File (Config_Parser'Class (Self));
+   begin
+      if V = "" then
+         return "";
+      elsif V (V'Last) = Directory_Separator then
+         return V;
+      else
+         return V & Directory_Separator;
+      end if;
+   end As_Absolute_Dir;
+
+   ----------
+   -- Open --
+   ----------
+
+   procedure Open (Self : in out File_Config_Parser; Filename : String) is
+      F   : Mapped_File;
+      Str : Str_Access;
+   begin
+      F := Open_Read (Filename);
+      Read (F);
+      Str := Data (F);
+
+      Self.Contents := To_Unbounded_String (String (Str (1 .. Last (F))));
+      Self.First    := 1;
+   end Open;
+
+   ------------
+   -- At_End --
+   ------------
+
+   overriding function At_End (Self : File_Config_Parser) return Boolean is
+   begin
+      return Self.First > Length (Self.Contents);
+   end At_End;
+
+   ----------
+   -- Open --
+   ----------
+
+   overriding procedure Open (Self : in out INI_Parser; Filename : String) is
+   begin
+      Open (File_Config_Parser (Self), Filename);
+      Self.Eol      := 0;
+   end Open;
+
+   ----------
+   -- Next --
+   ----------
+
+   overriding procedure Next (Self : in out INI_Parser) is
+      Equal : Integer;
+      Eol   : Integer;
+      Last  : constant Integer := Length (Self.Contents);
+   begin
+      --  Search end of current line
+      loop
+         Eol := Self.First;
+         Equal := 0;
+         while Eol <= Last
+           and then Element (Self.Contents, Eol) /= ASCII.LF
+         loop
+            if Equal = 0 and then Element (Self.Contents, Eol) = '=' then
+               Equal := Eol;
+            end if;
+            Eol := Eol + 1;
+         end loop;
+
+         Self.Eol   := Eol;
+         Self.Equal := Equal;
+
+         --  Are we seeing a comment ?
+
+         if Self.First + Length (Self.Comment_Start) - 1 <= Eol
+           and then Slice
+             (Self.Contents, Self.First,
+              Self.First + Length (Self.Comment_Start) - 1) =
+           Self.Comment_Start
+         then
+            null;
+
+         elsif Self.Use_Sections
+           and then Element (Self.Contents, Self.First) = '['
+           and then Element (Self.Contents, Self.Eol - 1) = ']'
+         then
+            Self.Current_Section := To_Unbounded_String
+              (Slice (Self.Contents, Self.First + 1, Self.Eol - 2));
+
+         elsif Equal /= 0 then
+            return;
+         end if;
+
+         Self.First := Eol + 1;
+      end loop;
+   end Next;
+
+   ---------------
+   -- Configure --
+   ---------------
+
+   procedure Configure
+     (Self             : in out INI_Parser;
+      Comment_Start    : String := "#";
+      Handles_Sections : Boolean := True)
+   is
+   begin
+      Self.Comment_Start := To_Unbounded_String (Comment_Start);
+      Self.Use_Sections  := Handles_Sections;
+   end Configure;
+
+   -------------
+   -- Section --
+   -------------
+
+   overriding function Section (Self : INI_Parser) return String is
+   begin
+      return To_String (Self.Current_Section);
+   end Section;
+
+   ---------
+   -- Key --
+   ---------
+
+   overriding function Key (Self : INI_Parser) return String is
+   begin
+      return Trim (Slice (Self.Contents, Self.First, Self.Equal - 1),
+                   Side => Ada.Strings.Both);
+   end Key;
+
+   -----------
+   -- Value --
+   -----------
+
+   overriding function Value (Self : INI_Parser) return String is
+   begin
+      return Trim (Slice (Self.Contents, Self.Equal + 1, Self.Eol - 1),
+                   Side => Ada.Strings.Left);
+   end Value;
+
+   ----------
+   -- Fill --
+   ----------
+
+   procedure Fill
+     (Self   : in out Config_Pool;
+      Config : in out Config_Parser'Class)
+   is
+   begin
+      Set_System_Id (Self, To_String (Config.System_ID));
+      while not At_End (Config) loop
+         Set (Self, Section (Config), Key (Config), Value (Config));
+         Next (Config);
+      end loop;
+   end Fill;
+
+   -------------------
+   -- Set_System_Id --
+   -------------------
+
+   procedure Set_System_Id (Self : in out Config_Pool; System_ID : String) is
+   begin
+      Self.System_ID := To_Unbounded_String (System_ID);
+   end Set_System_Id;
+
+   ---------
+   -- Get --
+   ---------
+
+   function Get
+     (Self    : Config_Pool;
+      Key     : String;
+      Section : String := Section_From_Key) return String is
+   begin
+      if Section = Section_From_Key then
+         for D in Key'Range loop
+            if Key (D) = '.' then
+               return Element (Self.Keys,
+                               Key (Key'First .. D - 1)
+                               & '#' & Key (D + 1 .. Key'Last));
+            end if;
+         end loop;
+
+         return Element (Self.Keys, '#' & Key);
+
+      else
+         return Element (Self.Keys, Section & "#" & Key);
+      end if;
+   end Get;
+
+   -----------------
+   -- Get_Integer --
+   -----------------
+
+   function Get_Integer
+     (Self    : Config_Pool;
+      Key     : String;
+      Section : String := Section_From_Key) return Integer is
+   begin
+      return Integer'Value (Get (Self, Key, Section));
+   end Get_Integer;
+
+   -----------------
+   -- Get_Boolean --
+   -----------------
+
+   function Get_Boolean
+     (Self    : Config_Pool;
+      Key     : String;
+      Section : String := Section_From_Key) return Boolean is
+   begin
+      return Boolean'Value (Get (Self, Key, Section));
+   end Get_Boolean;
+
+   --------------
+   -- Get_File --
+   --------------
+
+   function Get_File
+     (Self    : Config_Pool;
+      Key     : String;
+      Section : String := Section_From_Key) return String
+   is
+      Val : constant String := Get (Self, Key, Section);
+   begin
+      if Val = "" then
+         return "";
+      elsif Val (Val'First) = '/' then
+         return Val;
+      else
+         return Normalize_Pathname (Val, To_String (Self.System_ID));
+      end if;
+   end Get_File;
+
+   ---------
+   -- Set --
+   ---------
+
+   procedure Set (Self : in out Config_Pool; Section, Key, Value : String) is
+   begin
+      Include (Self.Keys, Section & "#" & Key, Value);
+   end Set;
+
+   --  Override a specific key
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create (Key : String; Section : String := "") return Config_Key is
+   begin
+      return Config_Key'(Section => To_Unbounded_String (Section),
+                         Key     => To_Unbounded_String (Key));
+   end Create;
+
+   ---------
+   -- Get --
+   ---------
+
+   function Get (Self : Config_Key; Conf : Config_Pool'Class) return String is
+   begin
+      return Get (Conf, To_String (Self.Section), To_String (Self.Key));
+   end Get;
+
+   --------------
+   -- Get_File --
+   --------------
+
+   function Get_File
+     (Self : Config_Key; Conf : Config_Pool'Class) return String is
+   begin
+      return Get_File (Conf, To_String (Self.Section), To_String (Self.Key));
+   end Get_File;
+
+end GNATCOLL.Config;
---
+++ src/gnatcoll-config.ads
@@ -0,0 +1,235 @@
+-----------------------------------------------------------------------
+--                          M O D E L I N G                          --
+--                                                                   --
+--                 Copyright (C) 2010, AdaCore                       --
+--                                                                   --
+-- GPS is free  software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
+--                                                                   --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this program; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
+-----------------------------------------------------------------------
+
+--  This package provides a general handling mechanism for config files.
+--  Any format of config files can be supported. The default implementation
+--  provides support for Windows-like .ini files, that is:
+--
+--  # Comment
+--  [Section]
+--  key1 = value1
+--  key2 = value2
+--
+--  This package is build through several layers of tagged objects:
+--  - the first layer provides the parsing of config files, and through a
+--    callback returns the (key, value) pairs to the application
+--  - the second layer provides a pool of these pairs, ie provides the storage
+--    on top of the first layer. Queries are done through strings
+--  - a third layer makes the keys as real objects, so that you build the key
+--    once, and then query the value from it directly. This is mostly syntactic
+--    sugar, although it helps ensure that you are always reading existing keys
+--    (if your coding convention forces users to use these keys).
+
+private with Ada.Containers.Indefinite_Hashed_Maps;
+private with Ada.Strings.Hash;
+private with Ada.Strings.Unbounded;
+
+package GNATCOLL.Config is
+
+   --------------------------
+   -- Parsing config files --
+   --------------------------
+
+   type Config_Parser is abstract tagged private;
+   --  Abstract type for all config streams (files, in-memory,...), with any
+   --  format. Concret types below will provide the actual implementation.
+   --  Typical usage looks like:
+   --     declare
+   --        C : File_Config_Parser;
+   --     begin
+   --        Open (C, "filename.txt");
+   --        while not C.At_End loop
+   --           Put_Line (C.Key & " = " & C.Value);
+   --           C.Next;
+   --        end loop;
+   --     end;
+
+   function At_End (Self : Config_Parser) return Boolean is abstract;
+   --  Whether the config parsing is at the end.
+
+   procedure Next (Self : in out Config_Parser) is abstract;
+   --  Move to the next (key, value) in the configuration. Before that call,
+   --  the parser is left on the first value in the configuration.
+
+   function Section (Self : Config_Parser) return String is abstract;
+   function Key (Self : Config_Parser) return String is abstract;
+   function Value (Self : Config_Parser) return String is abstract;
+   --  Return the current (section, key, value);
+
+   procedure Set_System_Id (Self : in out Config_Parser; System_ID : String);
+   --  Sets the system ID for the config.
+   --  If the config is found in a file, this should be the absolute path name
+   --  to that file. This will generally be called automatically when opening
+   --  the file.
+   --  This system id is used to resolve absolute file names.
+
+   function As_Integer       (Self : Config_Parser) return Integer;
+   function As_Boolean       (Self : Config_Parser) return Boolean;
+   function As_Absolute_File (Self : Config_Parser) return String;
+   function As_Absolute_Dir  (Self : Config_Parser) return String;
+   --  Assuming the current value is a file or directory, converts it to an
+   --  absolute name, where relative paths are resolved relative to the
+   --  config's system_id.
+   --  These will raise Constraint_Error if used on non-matching values.
+
+   -----------------
+   -- File config --
+   -----------------
+
+   type File_Config_Parser is abstract new Config_Parser with private;
+   --  A special implementation for config streams based on actual files.
+
+   procedure Open (Self : in out File_Config_Parser; Filename : String);
+   --  Open a file
+
+   overriding function At_End (Self : File_Config_Parser) return Boolean;
+
+   ---------------
+   -- INI files --
+   ---------------
+
+   type INI_Parser is new File_Config_Parser with private;
+   --  a special parser for Windows' .ini files
+
+   procedure Configure
+     (Self             : in out INI_Parser;
+      Comment_Start    : String := "#";
+      Handles_Sections : Boolean := True);
+
+   overriding procedure Open (Self : in out INI_Parser; Filename : String);
+   overriding procedure Next (Self : in out INI_Parser);
+   overriding function Section (Self : INI_Parser) return String;
+   overriding function Key (Self : INI_Parser) return String;
+   overriding function Value (Self : INI_Parser) return String;
+
+   -------------------
+   -- Resource pool --
+   -------------------
+
+   type Config_Pool is tagged private;
+   --  This type provides storage for a config file.
+
+   procedure Set_System_Id (Self : in out Config_Pool; System_ID : String);
+   --  Set the absolute name used to resolve file names in Get_File
+
+   procedure Fill
+     (Self   : in out Config_Pool;
+      Config : in out Config_Parser'Class);
+   --  Load all keys from Config, and store the (key, value) pairs in Self.
+   --  Multiple files can be merged into the same pool.
+   --  Set_System_Id is automatically called, thus file names will be resolved
+   --  relative to the last Config loaded in the pool.
+
+   Section_From_Key : constant String;
+   --  Indicates that the section should in fact be read from the key (as
+   --  opposed to being specified separately). In this case, the key is split
+   --  at the first "." (if there is none, the section name is empty).
+   --  For instance: "section1.key1" or "section1.key2".
+   --
+   --  It is often more convenient to specify the section that way, in exchange
+   --  for a small performance penalty and a possible ambiguity if the key
+   --  itself contains a ".", which is not recommended.
+
+   function Get (Self    : Config_Pool;
+                 Key     : String;
+                 Section : String := Section_From_Key) return String;
+   --  Return the value associated with Key.
+
+   function Get_Integer (Self    : Config_Pool;
+                         Key     : String;
+                         Section : String := Section_From_Key) return Integer;
+   function Get_Boolean (Self    : Config_Pool;
+                         Key     : String;
+                         Section : String := Section_From_Key) return Boolean;
+
+   function Get_File (Self    : Config_Pool;
+                      Key     : String;
+                      Section : String := Section_From_Key) return String;
+   --  Same as above, but returns an absolute filename. Relative paths are
+   --  resolved relative to the config location where Key was declared.
+
+   procedure Set (Self : in out Config_Pool; Section, Key, Value : String);
+   --  Override a specific key
+
+   --------------------------------
+   -- Resource pool, static keys --
+   --------------------------------
+
+   type Config_Key is tagged private;
+
+   function Create (Key : String; Section : String := "") return Config_Key;
+   --  Create a new config key
+
+   function Get (Self : Config_Key; Conf : Config_Pool'Class) return String;
+   function Get_File
+     (Self : Config_Key; Conf : Config_Pool'Class) return String;
+   --  Read the key from the configuration.
+   --  Using this API might help ensure that you are always accessing existing
+   --  keys. In this case, you would have a global package that defines all
+   --  valid keys:
+   --
+   --      Key1 : constant Config_Key := Create ("...");
+   --      Key2 : constant Config_Key := Create ("...");
+   --
+   --  Then your coding standard should specify that you can only access the
+   --  configuration via those keys:
+   --
+   --      Put_Line (Key1.Get);
+   --
+   --  There is therefore no possible typo in the name of the key, and if you
+   --  rename the key in the configuration file, you have a single place to
+   --  change.
+
+private
+   Section_From_Key : constant String := "#";
+
+   type Config_Parser is abstract tagged record
+      System_ID : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   type File_Config_Parser is abstract new Config_Parser with record
+      Contents : Ada.Strings.Unbounded.Unbounded_String;
+      First    : Integer;
+   end record;
+
+   type INI_Parser is new File_Config_Parser with record
+      Equal, Eol    : Integer;
+      Current_Section : Ada.Strings.Unbounded.Unbounded_String;
+      Comment_Start : Ada.Strings.Unbounded.Unbounded_String :=
+        Ada.Strings.Unbounded.To_Unbounded_String ("#");
+      Use_Sections  : Boolean := True;
+   end record;
+
+   package String_Maps is new Ada.Containers.Indefinite_Hashed_Maps
+     (Key_Type        => String,   --  "section#key"
+      Element_Type    => String,
+      Hash            => Ada.Strings.Hash,
+      Equivalent_Keys => "=",
+      "="             => "=");
+
+   type Config_Key is tagged record
+      Section, Key : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   type Config_Pool is tagged record
+      System_ID : Ada.Strings.Unbounded.Unbounded_String;
+      Keys      : String_Maps.Map;
+   end record;
+
+end GNATCOLL.Config;
