d0a0106c768ab058f3b0a97152b60899489a970f
Emmanuel Briot <briot@adacore.com>
2013-08-13 20:01:42 +0000

    Fix inorrect previous check in
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gps@214104 936e1b1b-40f2-da11-902a-00137254ae57

--- src/python/gnatcoll-scripts-python.adb
+++ src/python/gnatcoll-scripts-python.adb
@@ -2730,14 +2730,14 @@ package body GNATCOLL.Scripts.Python is
          --  ??? Temporarily commented out: it seems true with GPS, but
          --  fails on GMS.
 
---           if Active (Me) and then Get_CIR (Result) /= null then
---              Assert (Me,
---                      Get_Refcount (Object) = Old_Refcount + 1,
---                      "Get_CI should own a reference,"
---                      & Print_Refcount (Get_CIR (Result)) & " !="
---                      & Integer'Image (Old_Refcount + 1)'Img,
---                      Raise_Exception => False);
---           end if;
+         if False and then Active (Me) and then Get_CIR (Result) /= null then
+            Assert (Me,
+                    Get_Refcount (Object) = Old_Refcount + 1,
+                    "Get_CI should own a reference,"
+                    & Print_Refcount (Get_CIR (Result)) & " !="
+                    & Integer'Image (Old_Refcount + 1),
+                    Raise_Exception => False);
+         end if;
 
       elsif PyCObject_Check (Item) then
          CIR := PyCObject_AsVoidPtr (Item);
