61f6a3fe4f68c78fcdf43e06cba69a0c8a95832d
Pierre-Marie de Rodat <derodat@adacore.com>
2015-10-27 11:20:15 -0400

    Iconv: export a symbol which was missing from --without-iconv builds
    For --without-iconv builds, GNATCOLL.Iconv is nevertheless available and
    is supposed to be a mere stub. However this stub was missing a C symbol;
    as a result, programs using the corresponding procedure would not build
    properly. This change adds a function that exports this symbol in order
    to fix libiconv-less builds.
    
    Change-Id: I062f1c2f6829d01860d0a5f31ba6287b0bf2ea7e
    TN: OA27-033

---
+++ src/iconv/no_iconv/iconv_support.c
@@ -0,0 +1,13 @@
+/*
+ * Iconv binding support
+ * Copyright (C) 2015, AdaCore
+ */
+
+/* The following is a stub to make it possible to build GNATCOLL.Iconv without
+ * the actual Libiconv.  */
+
+void
+gnatcoll_iconv_set_locale (void)
+{
+  return;
+}
