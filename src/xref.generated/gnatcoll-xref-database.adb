2e39a698d6aded89300dc856c219f28ddbf17957
Justin Squirek <squirek@adacore.com>
2017-08-31 07:02:44 -0400

    Remove ineffective use clauses as per PA07-083

--- src/xref.generated/gnatcoll-xref-database.adb
+++ src/xref.generated/gnatcoll-xref-database.adb
@@ -2,7 +2,6 @@ with GNATCOLL.SQL.Inspect; use GNATCOLL.SQL.Inspect;
 with GNATCOLL.SQL.Exec; use GNATCOLL.SQL.Exec;
 package body GNATCOLL.Xref.Database is
    pragma Style_Checks (Off);
-   use type Cst_String_Access;
 
    function FK (Self : T_E2e'Class; Foreign : T_E2e_Kind'Class) return SQL_Criteria is
    begin
