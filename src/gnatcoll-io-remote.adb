66037b5d7bed3d4ba4b1f217a0805bb33e900a6a
Vadim Godunko <godunko@adacore.com>
2018-06-29 14:37:08 +0300

    R525-042 Remove unused local variable.
    Change-Id: I23c6372a296e5efe0ec233d527e7eafaaf9271b2

--- src/gnatcoll-io-remote.adb
+++ src/gnatcoll-io-remote.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                             G N A T C O L L                              --
 --                                                                          --
---                     Copyright (C) 2009-2017, AdaCore                     --
+--                     Copyright (C) 2009-2018, AdaCore                     --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
@@ -777,9 +777,8 @@ package body GNATCOLL.IO.Remote is
                   GNATCOLL.Path.Ensure_Directory
                     (Cur_Dir.Get_FS, Cur_Dir.Full.all);
       Content : GNAT.Strings.String_Access;
-      Dead    : Boolean;
       Written : Integer;
-      pragma Unreferenced (Dead, Written);
+      pragma Unreferenced (Written);
 
    begin
       Ensure_Initialized (File);
