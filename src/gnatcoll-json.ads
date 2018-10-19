c0138098fbb0afae1e8b4bf429197ac99be17373
Pierre-Marie de Rodat <derodat@adacore.com>
2018-10-19 11:09:45 +0200

    GNATCOLL.JSON: enhance in-spec documentation
    Change-Id: Ifea68e5be75889719a3038796bab9bf37b01e1f4

--- src/gnatcoll-json.ads
+++ src/gnatcoll-json.ads
@@ -21,6 +21,33 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+--  GNATCOLL.JSON exposes an API to parse and serialize data using the JSON
+--  (JavaScript Object Notation) format.
+--
+--  Parsing JSON is as easy as calling the Read function::
+--
+--     Data : JSON_Value := Read ("[1, ""foo"", {""foo"": null}]");
+--
+--  Encoding to JSON is not any more complex::
+--
+--     JSON_String : String := Write (Data);
+--
+--  JSON trees (JSON_Value) are available for both inspection and
+--  modification::
+--
+--     Float_Number : JSON_Value := Create (Float'(1.0));
+--     --  Mere float number
+--
+--     Object : JSON_Value := Get (Get (Data), 3);
+--     --  JSON object from Data: {"foo": null}
+--
+--     Some_Array : JSON_Value :=
+--        Create (Float_Number & Object & Create (False));
+--     --  Synthetic JSON array: [1.0, {"foo": null}, False]
+--
+--     --  Modify Data in place
+--     Data.Append (Some_Array);
+
 with Ada.Finalization;
 with Ada.Strings.Unbounded;
 with GNATCOLL.Strings;
@@ -32,12 +59,38 @@ package GNATCOLL.JSON is
 
    type JSON_Value_Type is
      (JSON_Null_Type,
+      --  Null value: all such JSON values are equivalent
+
       JSON_Boolean_Type,
+      --  Boolean value: either true or false
+
       JSON_Int_Type,
+      --  Integer value, encoded as an Ada Long_Long_Integer
+
       JSON_Float_Type,
+      --  Float value, encoded as an Ada Long_Float
+
       JSON_String_Type,
+      --  UTF-8 encoded string
+
       JSON_Array_Type,
-      JSON_Object_Type);
+      --  Array of JSON values
+
+      JSON_Object_Type
+      --  Sequence of fields. Each field has a unique name and maps to a
+      --  JSON value. Depending on the context, this sequence can be processed
+      --  as a mapping, because each field name is unique, but iterating on
+      --  fields is deterministic because it is a sequence underneath.
+   );
+   --  Each JSON value (JSON_Value below) has a specific kind...
+
+   subtype JSON_Elementary_Value_Type is JSON_Value_Type range
+     JSON_Null_Type .. JSON_String_Type;
+   --  Some are atoms...
+
+   subtype JSON_Container_Value_Type is JSON_Value_Type range
+     JSON_Array_Type .. JSON_Object_Type;
+   --  While others are containers for other values
 
    Invalid_JSON_Stream : exception;
 
@@ -47,20 +100,18 @@ package GNATCOLL.JSON is
    subtype UTF8_Unbounded_String is Ada.Strings.Unbounded.Unbounded_String;
    subtype UTF8_XString is GNATCOLL.Strings.XString;
 
-   subtype JSON_Elementary_Value_Type is JSON_Value_Type range
-     JSON_Null_Type .. JSON_String_Type;
-   subtype JSON_Container_Value_Type is JSON_Value_Type range
-     JSON_Array_Type .. JSON_Object_Type;
-
    type JSON_Value is tagged private;
-   --  Stores a JSON value, which can be either a simple type (integer,
-   --  string, ...) or an object with multiple fields, or an array.
+   --  Store a JSON value, which can be either a simple type (integer, string,
+   --  ...) or an object with multiple fields, or an array (see JSON_Value_Type
+   --  above).
    --
-   --  This type works by reference. Using the standard assignment operator
-   --  as in
+   --  This type has by-reference semantics, so using the standard assignment
+   --  operator as in::
+   --
    --      A := B;
-   --  means that modifying B will also modify A (and modifying A will of
-   --  course modify B).
+   --
+   --  just creates an alias. This means that modifying B will also modify A
+   --  (and modifying A will of course modify B).
    --
    --  If you want to create a separate copy, you must use the Clone function.
 
@@ -71,36 +122,59 @@ package GNATCOLL.JSON is
    JSON_Null : constant JSON_Value;
    Empty_Array : constant JSON_Array;
 
-   --  Array handling
+   --------------------
+   -- Array handling --
+   --------------------
+
    function Is_Empty (Arr : JSON_Array) return Boolean;
+   --  Return whether Arr is an empty array
+
    function Length (Arr : JSON_Array) return Natural;
+   --  Return the number of elements in Arr
+
    function Get (Arr : JSON_Array; Index : Positive) return JSON_Value;
+   --  If Arr has at least Index elements, return the element at that index.
+   --  Raise a Constraint_Error otherwise.
+
    procedure Append (Arr : in out JSON_Array; Val : JSON_Value);
+   --  Append Val as a new element at the end of the Arr array
+
    procedure Prepend (Arr : in out JSON_Array; Val : JSON_Value);
+   --  Insert Val as the first element of the Arr array
+
    procedure Clear (Arr : in out JSON_Array);
+   --  Remove all elements in Arr
+
    procedure Sort
-     (Arr : in out JSON_Array;
+     (Arr  : in out JSON_Array;
       Less : access function (Left, Right : JSON_Value) return Boolean);
-   --  Reorders the elements of array such that the elements are sorted
-   --  smallest first as determined by the strict comparison provided by
-   --  function Less.
+   --  Reorder the elements in Arr such that they are sorted smallest first
+   --  according to the strict comparison that Less implements.
 
    procedure Set_Element
      (Arr : in out JSON_Array; Index : Positive; Item : JSON_Value);
-   --  If Index is not in the array index range, then Constraint_Error is
-   --  propagated. Otherwise Set_Element assigns the value New_Item to the
-   --  element at position Index.
+   --  If Arr has at least Index elements, replace the element at that index
+   --  with Item. Raise a Constraint_Error otherwise.
 
-   function "&" (Arr : JSON_Array; Value : JSON_Value) return JSON_Array;
-   function "&" (Value1, Value2 : JSON_Value) return JSON_Array;
-   --  Create a new array. This is less efficient than Append because it
-   --  results in an extra copy of the array, but is easier to use when
+   --  Both functions below are less efficient than Append because they
+   --  result in an extra copy of the array, but they are easier to use when
    --  manipulating small arrays.
 
-   function Is_Empty (Val : JSON_Value) return Boolean;
-   --  Returns True if Val is empty array, empty object or null value
+   function "&" (Arr : JSON_Array; Value : JSON_Value) return JSON_Array;
+   --  Return a new array that appends Value to Arr
 
-   --  Read or write JSON values into strings
+   function "&" (Value1, Value2 : JSON_Value) return JSON_Array;
+   --  Return a new array that contains Value1 and Value2
+
+   function Is_Empty (Val : JSON_Value) return Boolean;
+   --  Return True if Val is empty array, empty object or null value. Return
+   --  False in all other cases.
+
+   ----------------------------------------------
+   -- Serialization/deserialization primitives --
+   ----------------------------------------------
+
+   --  TODO??? allow not to display the error on the standard output
 
    function Read
      (Strm     : Ada.Strings.Unbounded.Unbounded_String;
@@ -108,19 +182,27 @@ package GNATCOLL.JSON is
    function Read
      (Strm     : String;
       Filename : String := "<data>") return JSON_Value;
+   --  Parse the JSON document in Strm and return it. On parsing error, print
+   --  an error message referencing Filename on the standard output and raise
+   --  an Invalid_JSON_Stream exception.
+
    function Write (Item : JSON_Value; Compact : Boolean := True) return String;
    function Write (Item : JSON_Value; Compact : Boolean := True)
                    return Ada.Strings.Unbounded.Unbounded_String;
+   --  Return a string that encodes Item in JSON. Unless Compact is True, this
+   --  creates an indented multi-line representation.
 
-   --  Creation of JSON values
+   -----------------------------
+   -- Creation of JSON values --
+   -----------------------------
 
    function Create return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Null_Type);
-   --  Creates a 'null' JSON value
+   --  Create a 'null' JSON value
 
    function Create (Val : Boolean) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Boolean_Type);
-   --  Creates a boolean-typed JSON value
+   --  Create a boolean-typed JSON value
 
    function Create (Val : Integer) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Int_Type);
@@ -128,75 +210,78 @@ package GNATCOLL.JSON is
    pragma Postcondition (Kind (Create'Result) = JSON_Int_Type);
    function Create (Val : Long_Long_Integer) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Int_Type);
-   --  Creates an integer-typed JSON value
+   --  Create an integer-typed JSON value
 
    function Create (Val : Float) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Float_Type);
-   --  Creates a float-typed JSON value
+   --  Create a float-typed JSON value
 
    function Create (Val : Long_Float) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Float_Type);
 
    function Create (Val : UTF8_String) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_String_Type);
-   --  Creates a string-typed JSON value
+   --  Create a string-typed JSON value
 
    function Create (Val : UTF8_Unbounded_String) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_String_Type);
-   --  Creates a string-typed JSON value
+   --  Create a string-typed JSON value
 
    function Create (Val : UTF8_XString) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_String_Type);
-   --  Creates a string-typed JSON value
+   --  Create a string-typed JSON value
 
    function Create (Val : JSON_Array) return JSON_Value;
    pragma Postcondition (Kind (Create'Result) = JSON_Array_Type);
-   --  Creates a JSON value from the JSON array
+   --  Create a JSON value from the JSON array
 
    function Create_Object return JSON_Value;
    pragma Postcondition (Kind (Create_Object'Result) = JSON_Object_Type);
-   --  Creates an empty object. Values need to be added using the below
-   --  Set_Field methods
+   --  Create an empty object. Values need to be added using the below
+   --  Set_Field methods.
 
    procedure Sort
-     (Val : in out JSON_Value;
+     (Val  : in out JSON_Value;
       Less : access function (Left, Right : JSON_Value) return Boolean);
-   --  Reorders the elements of array or fields of object such that the
-   --  values are sorted smallest first as determined by the strict comparision
-   --  provided by function Less.
+   --  If Val is a JSON array or a JSON object, reorder its elements/fields
+   --  such that they are sorted smallest first according to the strict
+   --  comparison that Less implements.
 
    procedure Append (Arr : JSON_Value; Item : JSON_Value);
    pragma Precondition (Arr.Kind = JSON_Array_Type);
-   --  Append Arr only in case of it is an array, raise Constraint_Error
-   --  otherwise.
+   --  Assuming Arr is a JSON array, append Item to it
 
    function Clone (Val : JSON_Value) return JSON_Value;
-   --  Returns a deep clone of Val.
-   --  Any change in Val or its fields (recursively) will have no impact
-   --  on the resulting value.
+   --  Return a deep clone of Val.  Any later change in Val or its fields
+   --  (recursively) will have no impact on the resulting value.
 
    function "=" (Left, Right : JSON_Value) return Boolean;
-   --  Compare to values.
+   --  Return whether Left and Right are structurally identical.
+   --
    --  The actual contents is compared, not the pointers. So two objects
-   --  constructed independently, with the same contents, will match.
-   --  The order that fields were created is irrelevant, for objects.
-   --  The order in arrays is relevant.
+   --  constructed independently, with the same contents, will match. For JSON
+   --  objects, the order for fields is irrelevant, for objects. It is relevant
+   --  for arrays however.
 
    procedure Set_Field
      (Val        : JSON_Value;
       Field_Name : UTF8_String;
       Field      : JSON_Value);
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Adds or modifies the named field for the specified json object, using
-   --  the Field value.
+   --  Assuming Val is a JSON object, add a new field or modify the existing
+   --  one for the given Field_Name. The field value is Field afterwards.
 
    procedure Set_Field
      (Val        : JSON_Value;
       Field_Name : UTF8_XString;
       Field      : JSON_Value);
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Adds or modifies the named field for the specified json object, using
-   --  the Field value.
+   --  Assuming Val is a JSON object, add a new field or modify the existing
+   --  one for the given Field_Name. The field value is Field afterwards.
+
+   --  All the Set_Field overloads below are convenience shortcut that first
+   --  create a JSON value from their Field argument and then call the above
+   --  Set_Field procedures with the result.
 
    procedure Set_Field
      (Val        : JSON_Value;
@@ -245,7 +330,8 @@ package GNATCOLL.JSON is
       Field_Name : UTF8_String;
       Field      : JSON_Array);
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Any change you do to the array afterward will not impact Val
+   --  This performs a a shallow copy of Field, so any change you do to the
+   --  passed array for Field afterwards will not impact Val.
 
    procedure Set_Field_If_Not_Empty
      (Val        : JSON_Value;
@@ -267,17 +353,21 @@ package GNATCOLL.JSON is
       Field      : JSON_Array);
    pragma Precondition (Kind (Val) = JSON_Object_Type);
    --  Set Field only if it is not empty array.
-   --  Any change you do to the array afterward will not impact Val.
+   --  This performs a a shallow copy of Field, so any change you do to the
+   --  passed array for Field afterwards will not impact Val.
 
    procedure Unset_Field
      (Val        : JSON_Value;
       Field_Name : UTF8_String);
-   --  Unset the field with the given name, just as if Set_Field had never
-   --  been called.
+   --  Assuming Val is a JSON object, remove its field whose name matches
+   --  Field_Name. Do nothing if there is no such a field.
 
-   --  Utility functions used to translate a JSON value into an ordinary object
+   ------------------------------------------------------
+   -- Conversions from JSON values to native Ada types --
+   ------------------------------------------------------
 
    function Kind (Val : JSON_Value) return JSON_Value_Type;
+   --  Return the kind corresponding to the Val JSON value
 
    function Get (Val : JSON_Value) return Boolean;
    pragma Precondition (Kind (Val) = JSON_Boolean_Type);
@@ -311,7 +401,8 @@ package GNATCOLL.JSON is
 
    function Has_Field (Val : JSON_Value; Field : UTF8_String) return Boolean;
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Tell whether the object val contains a field named Field
+   --  Assuming Val is a JSON object, return whether it contains a field whose
+   --  name is Field.
 
    function Get (Val : JSON_Value; Field : UTF8_String) return JSON_Value;
    pragma Precondition (Kind (Val) = JSON_Object_Type);
@@ -366,7 +457,7 @@ package GNATCOLL.JSON is
      (Val : JSON_Value;
       CB  : access procedure (Name : UTF8_String; Value : JSON_Value));
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Iterate over all fields of the object
+   --  Assuming Val is a JSON object, call CB on all its fields
 
    generic
       type Mapped is private;
@@ -378,9 +469,8 @@ package GNATCOLL.JSON is
          Value       : JSON_Value);
       User_Object : in out Mapped);
    pragma Precondition (Kind (Val) = JSON_Object_Type);
-   --  Iter on all fields of the object, like Map_JSON_Object does,
-   --  but the callback can return a value which is also returned by
-   --  Gen_Map_JSON_Object itself.
+   --  Assuming Val is a JSON object, call CB on all its field, passing the
+   --  given User_Object from call to call.
 
 private
 
