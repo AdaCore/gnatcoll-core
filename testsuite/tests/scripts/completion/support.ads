a68cb093ef89f840cf82f5a69978af26e8fd299d
Vasiliy Fofanov <fofanov@adacore.com>
2018-11-25 23:26:13 +0100

    Add completion test for Scripts.
    Change-Id: I2e247ce8a8fafc4fc5f637908f542dee0f9d2ffc

---
+++ testsuite/tests/scripts/completion/support.ads
@@ -0,0 +1,9 @@
+with GNATCOLL.Scripts;
+
+package Support is
+
+   procedure Register_Functions (Repo : GNATCOLL.Scripts.Scripts_Repository);
+   --  Register the various scripting languages and the functions we export
+   --  to them
+
+end Support;
