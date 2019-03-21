8af4aa9b2ca2ce54029ecd3f2e2c5b528a3a7980
Pierre-Marie de Rodat <derodat@adacore.com>
2019-03-21 11:40:27 +0100

    Testsuite: forward gnatcov's output on coverage errors
    Change-Id: Ib8e4084a2f39c869a375dc49649fe33baeb89d42
    TN: S318-016

--- testsuite/drivers/gnatcov.py
+++ testsuite/drivers/gnatcov.py
@@ -1,10 +1,8 @@
+import logging
 import os
 
-from e3.fs import find
 from e3.os.process import Run
 from e3.testsuite.process import check_call
-
-from drivers import GNATCOLL_ROOT_DIR
 
 
 COVERAGE_LEVEL = 'stmt+decision'
@@ -36,38 +34,42 @@ def gnatcov_run(driver, cmd, test_name=None, result=None, **kwargs):
     cmd = ['gnatcov', 'run', '-o', trace_file, '-eargs'] + cmd
     result = check_call(driver, cmd, test_name, result, **kwargs)
 
-    Run(['gnatcov', 'coverage',
-         '--level={}'.format(COVERAGE_LEVEL),
-         '--scos=@{}'.format(driver.env.ali_files_list),
-         '--save-checkpoint={}'.format(checkpoint_file),
-         trace_file])
+    p = Run(['gnatcov', 'coverage',
+             '--level={}'.format(COVERAGE_LEVEL),
+             '--scos=@{}'.format(driver.env.ali_files_list),
+             '--save-checkpoint={}'.format(checkpoint_file),
+             trace_file])
+    if p.status:
+        logging.error('converting gnatcov trace file to checkpoint failed:\n'
+                      '{}'.format(p.out))
 
     return result
 
 
-def produce_report(output_dir, checkpoint_list):
+def produce_report(output_dir, checkpoint_list, src_dir):
     """Produce a coverage reports.
 
     :param str output_dir: Name of the directory to contain the DHTML coverage
         report.
     :param str checkpoint_list: Name of the file that contains the list of
         checkpoints to use.
+    :param str src_dir: Name of the directory that contains installed sources.
     """
-    # TODO: GNATcoverage is not be able to find the source file for a unit that
-    # is not used by any test program. This is a problem for units that are not
-    # tested at all. Let it know where to find the source file to avoid
-    # spurious warnings. Note that these units are reported as uncovered in any
-    # case.
-    src_dirs = set(
-        os.path.dirname(src_file)
-        for src_file in (
-            find(os.path.join(GNATCOLL_ROOT_DIR, 'src'), '*.ads')
-            + find(os.path.join(GNATCOLL_ROOT_DIR, 'src'), '*.adb')))
-
     args = ['gnatcov', 'coverage', '--annotate=dhtml',
             '--level={}'.format(COVERAGE_LEVEL),
             '--output-dir={}'.format(output_dir),
-            '--checkpoint=@{}'.format(checkpoint_list)]
-    for d in src_dirs:
-        args.append('--source-search={}'.format(d))
-    Run(args, output=None)
+            '--checkpoint=@{}'.format(checkpoint_list),
+
+            # TODO: GNATcoverage is not be able to find the source file for a
+            # unit that is not used by any test program. This is a problem for
+            # units that are not tested at all. Let it know where to find the
+            # source file to avoid spurious warnings. Note that these units are
+            # reported as uncovered in any case.
+            '--source-search={}'.format(src_dir)]
+    p = Run(args, output=None)
+    if p.status:
+        logging.error('could not produce the coverage report:\n'
+                      '{}'.format(p.out))
+    elif p.out:
+        logging.info('output of "gnatcov coverage" is not empty:\n'
+                     '{}'.format(p.out))
