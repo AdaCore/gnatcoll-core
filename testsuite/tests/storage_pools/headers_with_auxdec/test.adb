54cace463d27d7acc5595ee125f57b98d279ef88
Olivier Hainque <hainque@adacore.com>
2019-05-16 14:20:49 +0200

    Add storage_pools/headers_with_auxdec test for S507-006
    Change-Id: I11d29d82c028712714e272576e89c6b358720fe1

---
+++ testsuite/tests/storage_pools/headers_with_auxdec/test.adb
@@ -0,0 +1,37 @@
+--  Very simple test of GNATCOLL.Storage_Pools.Headers checking that it is
+--  useable together with DEC system extensions (S507-006).
+
+pragma Extend_System (Aux_DEC);
+
+with GNATCOLL.Storage_Pools.Headers;  use GNATCOLL.Storage_Pools.Headers;
+
+with Test_Assert;
+
+function Test return Integer is
+
+   package A renames Test_Assert;
+
+   type Header is record
+      Refcount : Natural := 0;
+   end record;
+   type Header_Access is access all Header;
+
+   package Pools is new Header_Pools (Header, Header_Access);
+
+   package String_Pools is new Pools.Typed (String);
+
+   Str : String_Pools.Element_Access;
+   Hdr : Header_Access;
+begin
+   --  Allocate element from the typed storage pool then retrieve
+   --  and access its header
+
+   Str := new String'("foo");
+   Hdr := String_Pools.Header_Of (Str);
+
+   String_Pools.Header_Of (Str).all.Refcount := 1;
+
+   A.Assert (Hdr.all.Refcount = 1, "unexpected ref count from extra header");
+
+   return A.Report;
+end Test;
