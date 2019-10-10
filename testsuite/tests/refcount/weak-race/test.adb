fcb29ce831f4245b0e510fee59613dc47dff911b
Pierre-Marie de Rodat <derodat@adacore.com>
2019-10-10 14:37:01 +0200

    refcount/weak-race: refactor testcase and skip with Valgrind
    Change-Id: I51628566f0c029421a1fe5b7bc6b41af8dca18de
    TN: S915-001

--- testsuite/tests/refcount/weak-race/test.adb
+++ testsuite/tests/refcount/weak-race/test.adb
@@ -14,13 +14,33 @@ function Test return Integer is
       entry Stop;
    end Weak_Tester;
 
-   subtype Processing_Range is Integer range -10_000 .. 10_000;
+   --  This test runs three ref-to-weak and then weak-to-ref patterns in loops
+   --  concurrently to check that both conversions are resilient to concurrent
+   --  inc-ref/dec-ref operations.
+   --
+   --  This is an attempt to check that there is no race condition between
+   --  ref-counting drop to 0 happens and a weak-to-ref conversion.
+   --
+   --  The structure of this test is the following:
+   --
+   --    * Some code runs a weak-to-ref conversion at most Task_Inner_Range
+   --      times, stopping as soon as the conversion returns a null reference.
+   --
+   --    * Run three tasks (Weak_Tester) run that code Task_Outer_Range times.
+   --
+   --  We expect race conditions to somehow make the weak-to-ref conversion
+   --  produce a stale reference (which is invalid), so that dereferencing it
+   --  would yield an unexpected number.
+
+   subtype Task_Outer_Range is Positive range 1 .. 20_000;
+   subtype Task_Inner_Range is Natural;
+   Notification_Interval : constant Positive := 10_000;
 
    task body Weak_Tester is
       Stop_It : Boolean := False;
       W : Weak_Ref;
    begin
-      Main_Loop : for J in Processing_Range loop
+      Main_Loop : for J in Task_Outer_Range loop
          declare
             R : Ref;
          begin
@@ -39,7 +59,7 @@ function Test return Integer is
 
          exit Main_Loop when Stop_It;
 
-         for K in Natural'Range loop
+         for K in Task_Inner_Range'Range loop
             declare
                use Ada.Task_Identification;
                R : Ref;
@@ -52,7 +72,7 @@ function Test return Integer is
                         & Image (Current_Task));
                   end if;
 
-                  if J rem 9999 = 0 then
+                  if J mod Notification_Interval = 0 then
                      Put_Line (J'Img & K'Img);
                   end if;
 
@@ -76,7 +96,7 @@ function Test return Integer is
    Stamp : constant Time := Clock;
 
 begin
-   for J in Processing_Range loop
+   for J in Task_Outer_Range loop
       declare
          R : Ref;
       begin
