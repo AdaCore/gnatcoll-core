700a64fa77694a72eafc70784b6de38cbe0ab1d7
Vincent Jicquel <jicquel@adacore.com>
2023-04-13 16:51:56 +0200

    Implement Symbolic_Link_Is_Internal
    The symlink / symbolic link is a special file, whose purpose is to point
    to a file or a directory, called the target.
    
    Symbolic_Link_Is_Internal indicates if a symbolic link target is external
    to the specified base directory. A link target is external in
    several cases:
    - Link target is an absolute path.
    - Link target is a relative path pointing directly out of the
    base directory.
    - Link target is a relative path which leaves the base
    directory at least once.
    
    Not supported on Windows.
    
    Tests have been implemented.
    
    Issue: #14
    
    TN: W329-010
    Change-Id: I3c5cf798d12f5196e12af37241cc3029ec42f054

--- src/os/gnatcoll-os-fsutil.adb
+++ src/os/gnatcoll-os-fsutil.adb
@@ -22,8 +22,12 @@
 ------------------------------------------------------------------------------
 
 with Ada.Unchecked_Deallocation;
-with GNATCOLL.OS.Stat; use GNATCOLL.OS.Stat;
+with GNATCOLL.OS.Stat;      use GNATCOLL.OS.Stat;
+with GNATCOLL.OS.Constants;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with GNAT.OS_Lib;
+with GNATCOLL.Strings_Impl;
+with Ada.Characters.Handling;
 
 package body GNATCOLL.OS.FSUtil is
 
@@ -196,5 +200,152 @@ package body GNATCOLL.OS.FSUtil is
    function Read_Symbolic_Link
      (Link_Path : UTF8.UTF_8_String; Target_Path : out Unbounded_String)
       return Boolean is separate;
+
+   -------------------------------
+   -- Symbolic_Link_Is_Internal --
+   -------------------------------
+
+   function Symbolic_Link_Is_Internal
+     (Top_Dir_Path : UTF8.UTF_8_String; Link_Path : UTF8.UTF_8_String)
+      return Boolean
+   is
+      Target_Path : Unbounded_String;
+
+   begin
+
+      if not Read_Symbolic_Link (Link_Path, Target_Path) then
+         return False;
+      end if;
+
+      if GNAT.OS_Lib.Is_Absolute_Path (To_String (Target_Path)) then
+         return False;
+      end if;
+
+      --  Concatenate Link_Path and Target_Path split by split, and see if
+      --  it goes outside Top_Dir.
+
+      --  Example top dir: /tmp/dir and target_path: dir1/../../dir/fileA
+      --  In order:
+      --  - /tmp/dir/dir1: OK
+      --  - /tmp/dir/dir1/..: OK
+      --  - /tmp/dir/dir1/../..: KO!
+
+      declare
+         use Ada.Characters.Handling;
+         package Strings is new GNATCOLL.Strings_Impl.Strings
+           (GNATCOLL.Strings_Impl.Optimal_String_Size, Character, String);
+         use Strings;
+
+         package Constants renames GNATCOLL.OS.Constants;
+
+         function Normalize_Link_Pathname (Link_Path : String) return String;
+         --  Obtain link normalized pathname without resolving the link itself
+
+         function Normalize_Link_Pathname (Link_Path : String) return String is
+            Normalized_Link_Path : constant String :=
+              GNAT.OS_Lib.Normalize_Pathname
+                (Link_Path, Resolve_Links => False);
+            X                    : Strings.XString;
+         begin
+            X.Set (Normalized_Link_Path);
+
+            declare
+               X_Arr : constant Strings.XString_Array :=
+                 X.Right_Split
+                   (Sep        => Constants.Dir_Sep, Max_Split => 2,
+                    Omit_Empty => True);
+               --  Extract symbolic link basename, and base path
+            begin
+
+               if X_Arr'Length = 1 then
+
+                  --  Can occur if the link is at the top of a file system.
+
+                  return Constants.Dir_Sep & X_Arr (1).To_String;
+               else
+
+                  --  Resolve symbolic links if needed, and normalize path
+                  --  to final link directory.
+
+                  return
+                    GNAT.OS_Lib.Normalize_Pathname (X_Arr (2).To_String) &
+                    Constants.Dir_Sep & X_Arr (1).To_String;
+               end if;
+            end;
+         end Normalize_Link_Pathname;
+
+         Normalized_Top_Dir   : constant String :=
+           GNAT.OS_Lib.Normalize_Pathname (Top_Dir_Path);
+         Normalized_Link_Path : constant String :=
+           Normalize_Link_Pathname (Link_Path);
+         Link_Depth           : Integer         := 0;
+         XS                   : Strings.XString;
+      begin
+
+         --  Check if the link is outside the top directory
+         --  First case: Normalized link path is simply shorter than
+         --  the top directory one.
+         --  Second case: Normalized top directory path is not contained in
+         --  normalized link path.
+
+         if Normalized_Link_Path'Length < Normalized_Top_Dir'Length
+           or else
+             Normalized_Link_Path
+               (Normalized_Link_Path'First ..
+                    Normalized_Link_Path'First + Normalized_Top_Dir'Length -
+                    1) /=
+             Normalized_Top_Dir
+         then
+            return False;
+         end if;
+
+         --  Compute the link depth in the top directory
+
+         XS.Set
+           (Normalized_Link_Path
+              (Normalized_Top_Dir'Last + 1 .. Normalized_Link_Path'Last));
+
+         Link_Depth := 0;
+
+         declare
+            First_Item : Boolean := True;
+         begin
+            for Dir_Entry of XS.Split
+              (GNATCOLL.OS.Constants.Dir_Sep, Omit_Empty => True)
+            loop
+               if First_Item then
+                  First_Item := False;
+
+                  --  The last XS.Split element is the link itself.
+                  --  It shall not count in depth.
+
+               else
+                  Link_Depth := Link_Depth + 1;
+               end if;
+            end loop;
+         end;
+
+         --  Then, concatenate each subpart of the target path relative to the
+         --  link, and see if depth becomes negative once. In this case, the
+         --  target link went outside of the directory.
+
+         XS.Set (To_String (Target_Path));
+         for Dir_Entry of XS.Split
+           (GNATCOLL.OS.Constants.Dir_Sep, Omit_Empty => True)
+         loop
+            if Dir_Entry = ".." then
+               Link_Depth := Link_Depth - 1;
+            elsif Dir_Entry /= "." then
+               Link_Depth := Link_Depth + 1;
+            end if;
+
+            if Link_Depth < 0 then
+               return False;
+            end if;
+         end loop;
+      end;
+
+      return True;
+   end Symbolic_Link_Is_Internal;
 
 end GNATCOLL.OS.FSUtil;
