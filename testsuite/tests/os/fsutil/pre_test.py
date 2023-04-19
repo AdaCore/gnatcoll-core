0ec4d57e55b43a75c24fefae0ac6ff4fa7384fdf
Vincent Jicquel <jicquel@adacore.com>
2023-04-19 14:47:23 +0200

    Fix Copy_File_Content implementation for linux 32 bits
    Copy_File_Content failed on 32 bits systems. Ada Send_File always returned
    64 bits results, even on 32 bits systems, while the C function
    __gnatcoll_sendfile could return 32 or 64 bits, depending on the length of
    ssize_t.
    
    This commit fixes this issue. Send_File now takes types which depend on the
    system memory size. Moreover, sendfile maximum count constraint is now handled
    directly by Ada.
    
    FSUtil Test has been modified to use file of a bit more than 4GiB,
    which requires 33 bits.
    
    TN: W417-016

--- testsuite/tests/os/fsutil/pre_test.py
+++ testsuite/tests/os/fsutil/pre_test.py
@@ -3,8 +3,8 @@ import os
 
 # Sendfile is used by Copy_File in Linux implementation. It has a 2GiB limit.
 # A file which weights more than this limit shall work properly, as several
-# call to Sendfile will be made.
-# Ini
-fileSizeInBytes = 2*1024*1024*1024 + 1024*1024
+# call to Sendfile will be made. Moreover, we want also to test 32 bits systems
+# with a file whose weight overcomes 32 bits.
+fileSizeInBytes = 4*1024*1024*1024 + 1024*1024
 with open('huge_file', 'wb') as fout:
     fout.write(os.urandom(fileSizeInBytes))
