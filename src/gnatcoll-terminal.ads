724c8dba486c913075858e21074223940795f22f
fastrgv <fastrgv@gmail.com>
2023-10-19 08:01:37 -0700

    Add functionality to access terminal window height (number of lines)
    Provide a Get_Lines function in GNATCOLL.Terminal.
    
    Github PR: https://github.com/AdaCore/gnatcoll-core/pull/81

--- src/gnatcoll-terminal.ads
+++ src/gnatcoll-terminal.ads
@@ -140,6 +140,10 @@ package GNATCOLL.Terminal is
    --  Return the width of the terminal, or -1 if that width is either
    --  unknown or does not apply (as is the case for files for instance).
 
+   function Get_Lines (Self : Terminal_Info) return Integer;
+   --  Return the height of the terminal, or -1 if that height is either
+   --  unknown or does not apply (as is the case for files for instance).
+
    -----------
    -- Utils --
    -----------
