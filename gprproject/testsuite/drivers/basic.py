773ec59e5b7d64f6fb2c015b526824962e0d792a
Nicolas Roche <roche@adacore.com>
2026-03-19 21:48:28 +0100

    Revert "Refactor testsuite code"
    This reverts commit 156c81b247e1eea41c40f30e994de6439466b160.

--- gprproject/testsuite/drivers/basic.py
+++ gprproject/testsuite/drivers/basic.py
@@ -5,8 +5,8 @@ from e3.fs import cp, mkdir
 from e3.os.fs import df
 from e3.testsuite.driver.classic import ClassicTestDriver
 from e3.testsuite.control import YAMLTestControlCreator
+from e3.testsuite.process import check_call
 from . import gprbuild, run_test_program
-import subprocess
 
 
 class BasicTestDriver(ClassicTestDriver):
@@ -85,12 +85,11 @@ class BasicTestDriver(ClassicTestDriver):
 
         pre_test_py = os.path.join(self.test_env["test_dir"], "pre_test.py")
         if os.path.isfile(pre_test_py):
-            proc = subprocess.run(
+            check_call(
+                self,
                 [interpreter(), pre_test_py],
                 cwd=self.test_env["working_dir"],
                 timeout=self.default_process_timeout,
-                text=True,
-                capture_output=True
             )
 
         # Run the test program
@@ -115,19 +114,13 @@ class BasicTestDriver(ClassicTestDriver):
 
         post_test_py = os.path.join(self.test_env["test_dir"], "post_test.py")
         if os.path.isfile(post_test_py):
-            proc = subprocess.run(
+            check_call(
+                self,
                 [interpreter(), post_test_py],
                 cwd=self.test_env["working_dir"],
                 timeout=self.default_process_timeout,
-                input=self.output.log.encode("utf-8"),
-                text=False,
-                capture_output=True
+                input=f"|{self.output}"
             )
-
-            # Append post_test.py output to the expected test log. It can be
-            # useful for adding the success marker for instance.
-            self.result.log.log += proc.stdout.decode("utf-8")
-            self.result.log.log += proc.stderr.decode("utf-8")
 
     def compute_failures(self):
         return (
