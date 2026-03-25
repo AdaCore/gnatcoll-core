1b50ee0280aaf4f37a37f042b916a015a25b4432
Vincent Jicquel <jicquel@adacore.com>
2026-03-25 18:06:25 +0100

    Add suppression exception for valgrind for ada environment
    The subprogram Ada.Environment_Variables.Set produces potential leaks
    that we do not want to process in the gnatcoll testsuite.
    
    Part-Of: eng/toolchain/gnatcoll-core#75

--- gprproject/testsuite/drivers/valgrind.py
+++ gprproject/testsuite/drivers/valgrind.py
@@ -1,5 +1,5 @@
 from . import bin_check_call
-
+import os
 
 def check_call_valgrind(driver, cmd, slot, test_name=None, result=None, **kwargs):
     """
@@ -7,6 +7,14 @@ def check_call_valgrind(driver, cmd, slot, test_name=None, result=None, **kwargs
     this is a Valgrind-checked testsuite run. The process exit status will be
     2 if Valgrind finds memory issues.
     """
+
     if driver.env.valgrind:
-        cmd = ["valgrind", "-q", "--error-exitcode=2", "--leak-check=full"] + cmd
+        cmd = [
+            "valgrind",
+            "-q",
+            "--error-exitcode=2",
+            "--leak-check=full",
+            "--suppressions="
+            + os.path.join(os.path.dirname(os.path.abspath(__file__)), "valgrind.supp"),
+        ] + cmd
     return bin_check_call(driver, cmd, slot, test_name, result, **kwargs)
